46befe71fd6ef463bd7d865b1fed107cb9b2b05f
kazvee <kazveedev@gmail.com>
2024-03-13 15:37:29 -0600

    feat: add reducer logic for ADD_BESTIE action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
+import { ADD_BESTIE } from '../actions';
 import besties_json from '../data/besties.json';
 
 function besties(state = besties_json, action) {
   switch (action.type) {
+    case ADD_BESTIE:
+      let besties = state.filter((item) => item.id !== action.id);
+      return besties;
     default:
       return state;
   }
