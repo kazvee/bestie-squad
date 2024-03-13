75f2affde5379af79516d8b82c20cc9937e7a68a
kazvee <kazveedev@gmail.com>
2024-03-13 15:20:45 -0600

    feat: set up Redux store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import rootReducer from './reducers';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+console.log('store.getState()', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
