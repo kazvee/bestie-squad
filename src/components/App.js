dc74e5c703e59eb0925ab1fbb89468b0160bab5d
kazvee <kazveedev@gmail.com>
2024-03-13 14:41:09 -0600

    chore: set up basic project structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bestie Squad 😻</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
