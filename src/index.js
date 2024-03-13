504c831df8b019398fb7105a297101cb13c2a1e7
kazvee <kazveedev@gmail.com>
2024-03-13 15:39:16 -0600

    feat: implement Redux action and dispatch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { addBestieById } from './actions';
 import App from './components/App';
 import rootReducer from './reducers';
 
@@ -8,6 +9,8 @@ const store = configureStore({
   reducer: rootReducer,
 });
 console.log('store.getState()', store.getState());
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(addBestieById(4));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
