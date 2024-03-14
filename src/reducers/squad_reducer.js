5720bcbb5efd01498b338eb959bbd14572f3d1fe
kazvee <kazveedev@gmail.com>
2024-03-14 12:32:05 -0600

    feat: add functionality in squad reducer for removing bestie

--- src/reducers/squad_reducer.js
+++ src/reducers/squad_reducer.js
@@ -1,10 +1,13 @@
-import { ADD_BESTIE } from '../actions';
+import { ADD_BESTIE, REMOVE_BESTIE } from '../actions';
 import { createBestie } from '../reducers/helpers';
 
 function squad(state = [], action) {
   switch (action.type) {
     case ADD_BESTIE:
       let squad = [...state, createBestie(action.id)];
+      return squad;
+    case REMOVE_BESTIE:
+      squad = state.filter((item) => item.id !== action.id);
       return squad;
     default:
       return state;
