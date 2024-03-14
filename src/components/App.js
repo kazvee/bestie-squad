b58d6d1bf8e2deba9e4c9431ef0c247c0465bd5e
kazvee <kazveedev@gmail.com>
2024-03-14 11:54:33 -0600

    style: update header styling

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <div>
+        <div className='header-container'>
           <h1>Bestie Squad</h1>
           <img
             className='header-image'
