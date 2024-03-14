fffc7cb6b6279aee676286fef5789c1e70e4bbae
kazvee <kazveedev@gmail.com>
2024-03-14 12:21:23 -0600

    feat: add action creator for removing bestie

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
 export const ADD_BESTIE = 'ADD_BESTIE';
+export const REMOVE_BESTIE = 'REMOVE_BESTIE';
 
 export function addBestieById(id) {
   const action = {
     type: ADD_BESTIE,
+    id,
+  };
+  return action;
+}
+
+export function removeBestieById(id) {
+  const action = {
+    type: REMOVE_BESTIE,
     id,
   };
   return action;
