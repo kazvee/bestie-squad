fb5d4d014d38ce56360c5dc8de18ccc92a131bf3
kazvee <kazveedev@gmail.com>
2024-03-14 10:19:57 -0600

    feat: implement SquadList component in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import '../styles/index.css';
 import BestiesList from './BestiesList';
 import Footer from './Footer';
+import SquadList from './SquadList';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className='App'>
         <h1>Bestie Squad 😻</h1>
         <BestiesList />
+        <SquadList />
         <Footer />
       </div>
     );
