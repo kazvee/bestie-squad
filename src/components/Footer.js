d52c50092265db53ec13a39d50979a30f82de89d
kazvee <kazveedev@gmail.com>
2024-03-14 10:03:38 -0600

    feat: add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <span>
+        Made with{' '}
+        <a href='https://github.com/kazvee/bestie-squad' rel='nofollow'>
+          <img
+            width='24'
+            height='24'
+            src='https://img.icons8.com/doodle/48/stitched-heart.png'
+            alt='stitched-heart'
+          />
+        </a>{' '}
+        and:
+      </span>
+      <span>
+        <a
+          href='https://www.favicon.cc/?action=icon&file_id=996246'
+          rel='nofollow'
+        >
+          Favicon
+        </a>{' '}
+        by{' '}
+        <a
+          href='https://www.favicon.cc/?action=icon_list&user_id=673951'
+          rel='nofollow'
+        >
+          unsureamen
+        </a>
+      </span>
+      <span>
+        <a
+          href='https://www.freepik.com/free-vector/gradient-pastel-sky-background_13326145.htm'
+          rel='nofollow'
+        >
+          Background image
+        </a>{' '}
+        by{' '}
+        <a href='https://www.freepik.com' rel='nofollow'>
+          Freepik
+        </a>
+      </span>
+      <span>
+        <a href='https://icons8.com/icon/81747/heart' rel='nofollow'>
+          Heart
+        </a>{' '}
+        &{' '}
+        <a href='https://icons8.com/icon/80705/add' rel='nofollow'>
+          Add
+        </a>{' '}
+        icons by{' '}
+        <a href='https://icons8.com/' rel='nofollow'>
+          Icons8
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
