490867c23b30ef1717f5c7bfd9bbd2cc506cca53
kazvee <kazveedev@gmail.com>
2024-03-14 13:52:46 -0600

    feat: add total charm level to SquadStats

--- src/components/SquadStats.js
+++ src/components/SquadStats.js
@@ -18,15 +18,22 @@ class SquadStats extends Component {
     return friendship;
   }
 
+  charm() {
+    let charm = 0;
+    this.props.squad.forEach((squaddie) => {
+      charm += squaddie.charm;
+    });
+    return charm;
+  }
+
   render() {
     return (
       <div>
         <h2>Squad Stats</h2>
         <ul>
-          <li className='list-group-item'>Total Cuteness: {this.cuteness()}</li>
-          <li className='list-group-item'>
-            Total Friendship: {this.friendship()}
-          </li>
+        <li className='list-group-item'>Total Cuteness: {this.cuteness()}</li>
+          <li className='list-group-item'>Total Friendship: {this.friendship()}</li>
+          <li className='list-group-item'>Total Charm: {this.charm()}</li>
         </ul>
       </div>
     );
