7f1b22fd8f7ed8da8e8a6b804115e1d395c9c8f8
kazvee <kazveedev@gmail.com>
2024-03-14 12:34:48 -0600

    feat: add removal functionality in besties reducer

--- src/reducers/besties_reducer.js
+++ src/reducers/besties_reducer.js
@@ -1,10 +1,14 @@
-import { ADD_BESTIE } from '../actions';
+import { ADD_BESTIE, REMOVE_BESTIE } from '../actions';
 import besties_json from '../data/besties.json';
+import { createBestie } from './helpers';
 
 function besties(state = besties_json, action) {
   switch (action.type) {
     case ADD_BESTIE:
       let besties = state.filter((item) => item.id !== action.id);
+      return besties;
+    case REMOVE_BESTIE:
+      besties = [...state, createBestie(action.id)];
       return besties;
     default:
       return state;
