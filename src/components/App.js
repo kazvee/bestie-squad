911757b2ec283e84582873bc3cd07578fa6a1ba5
kazvee <kazveedev@gmail.com>
2024-03-14 11:53:35 -0600

    style: update header image

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,16 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Bestie Squad 😻</h1>
+        <div>
+          <h1>Bestie Squad</h1>
+          <img
+            className='header-image'
+            width='64'
+            height='64'
+            src='https://img.icons8.com/dusk/64/sylvanian-families.png'
+            alt='bestie squad logo'
+          />
+        </div>
         <div className='col-md-6'>
           <BestiesList />
         </div>
